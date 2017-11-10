5d1c21731d209d1d779ffdf57bf27b8106446eb9
austinmoney <austinmoney@gmail.com>
2017-11-10 10:25:31 -0700

    timer fully functional

--- WatchApp/ViewController.swift
+++ WatchApp/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 import AudioToolbox
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
+    
+    // MARK: - IBOutlets and IBActions
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var button: UIButton!
-    
-//    var time: MyTimer? {
-//        didSet {
-////            updateTimerLabel()
-//        }
-//    }
     
     @IBAction func buttonTapped(_ sender: Any) {
         if isTimerRunning == false {
@@ -28,56 +24,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         } else if isTimerRunning == true { //if timer is running
             timer.invalidate()
             isTimerRunning = false
-            seconds = 60
+            
             label.text = "\(seconds)"
         }
-//        if isTimerRunning == true {
-//            stopTimer()
-//        } else {
-//            runTimer(selectedTime: Int(selectedTime))
-////            isTimerRunning = true
-//        }
-//        setView()
     }
     
-//    var selectedTime: TimeInterval {
-//        return TimeInterval(pickerView.selectedRow(inComponent: 0))
-//    }
-//    var timeRemaining: TimeInterval = 1
-//    var timer = Timer()
-//    var isTimerRunning = false
-//
-//    func runTimer(selectedTime: Int) {
-//        timer = Timer.scheduledTimer(timeInterval: TimeInterval(selectedTime), target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
-//
-//    }
-//
-//    func stopTimer() {
-//        timer.invalidate()
-//
-//        // Update UI
-//
-//    }
-//
-//
-//    @objc func updateTimer() {
-//        if timeRemaining < 1 {
-//            timer.invalidate()
-//            //Send alert to indicate time's up.
-//        } else {
-//            timeRemaining -= 1
-////            label.text = timeString(time: TimeInterval(timeRemaining))
-//            label.text = String(timeRemaining)
-//            print(timeRemaining)
-//            //            labelButton.setTitle(timeString(time: TimeInterval(seconds)), for: UIControlState.normal)
-//        }
-//    }
-//
-    let array = Array(1...60)
-    var seconds = 5
+    // MARK: - Properties
+    
+//    let array = Array(1...60)
+    let array = [1,5,10,15,20,25,30,35,40,45,50,55,60]
+    var seconds = 5.0
     var timer = Timer()
     var isTimerRunning = false
     var resumeTapped = false
+    
+    // MARK: - Functions
     
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
@@ -87,30 +48,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         if seconds == 0 {
             timer.invalidate()
             AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
-            label.text = "5"
+            seconds = Double(array[pickerView.selectedRow(inComponent: 0)])
+            label.text = "\(seconds)"
             runTimer()
-            seconds = 5
         } else {
             seconds -= 1
             label.text = "\(seconds)"
+        }
     }
-    }
-//
-//    let myTimer = MyTimer()
-//
-//    func setView() {
-////        updateTimerLabel()
-//        // If timer is running, start button title should say "Cancel". If timer is not running, title should say "Start nap"
-//        if isTimerRunning == true {
-//            button.setTitle("Cancel", for: UIControlState())
-//        } else {
-//            button.setTitle("Start", for: UIControlState())
-//        }
-//    }
+    
     
     @objc func updateTimerLabel(notification: Notification) {
-//        label.text = String(timeRemaining)
+        //        label.text = String(timeRemaining)
     }
+    
+    // MARK: - PickerView
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -120,18 +72,16 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         return String(array[row])
     }
     
-//    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
-//        return NSString("Seconds")
-//    }
-    
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return array.count
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         label.text = String(array[row])
+        seconds = Double(array[row])
     }
+    
+    // MARK: - Override Func
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -139,16 +89,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         NotificationCenter.default.addObserver(self, selector: #selector(updateTimerLabel(notification:)), name: Notification.Name("timeRemainingDidChange"), object: nil)
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-//    @objc func updateLabel(notification: Notification) {
-//        label.text = String(myTimer.timeRemaining)
-//    }
-
-
 }
 
